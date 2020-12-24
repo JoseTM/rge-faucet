8a196ea87f6dc21f450896e3b5dc1cb00282cae8
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2020-12-24 16:25:00 +0000

    master

--- store/index.js
+++ store/index.js
@@ -1,7 +1,6 @@
-
 export const state = () => ({
   rgeAt: {
-    3: '0x32C3f2Ca80677Ccb67AB393d4a2429f47c94A3f3',
+    3: '0x3D4d6B701376E37C599B70FF8fad2C2D9C05DC8B',
     77: '0x5475300766433dd082a7340fc48a445c483df68f'
   },
 })
